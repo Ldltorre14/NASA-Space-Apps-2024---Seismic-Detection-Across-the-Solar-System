bbc96fa1f831792b8734d87b274bcbdd5f8cbe2b
Ldltorre14 <lopdlt.learning@gmail.com>
2024-10-06 11:49:47 -0600

    Finished the normalization script with MinMaxScaler

--- src/utils/normalize.py
+++ src/utils/normalize.py
@@ -1,8 +1,9 @@
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
 
 #Function for normalizing the speed, so the range goes from 0 to 1 instead of a bigger range
 def normalizeVelocity(data):
-    scaler = StandardScaler()
+    print("Normalizing Velocity...")
+    scaler = MinMaxScaler()
     data['velocity(m/s)'] = scaler.fit_transform(data[['velocity(m/s)']])
     
     return data 
